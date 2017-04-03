f7441d228442ded5aa66d691a53ea886dcc23af0
unknown <VMALMAGRO@CONTAB_32.SMARAPD.COM.BR>
2017-04-03 14:02:47 -0300

    acrecentando a primeira parte do algoritmo de busca

--- deepsearch.js
+++ deepsearch.js
@@ -1,26 +1,6 @@
-var elementos = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-
-           /* A  B  C  D  E  F  G */
-var linhaA = [0, 1, 1, 0, 1, 0, 0];
-var linhaB = [1, 0, 0, 1, 0, 1, 0];
-var linhaC = [1, 0, 0, 0, 0, 0, 1];
-var linhaD = [0, 1, 0, 0, 0, 0, 0];
-var linhaE = [1, 0, 0, 0, 0, 1, 0];
-var linhaF = [0, 1, 0, 0, 1, 0, 0];
-var linhaG = [0, 0, 1, 0, 0, 0, 0];
-
- var linha1 = factory('A', linhaA);
- var linha2 = factory('B', linhaB);
- var linha3 = factory('C', linhaC);
- var linha4 = factory('D', linhaD);
- var linha5 = factory('E', linhaE);
- var linha6 = factory('F', linhaF);
- var linha7 = factory('G', linhaG);
-
 var factory = (elemento, linha) => {
 
-    let anterior = [];
-    let proximo  = [];
+    let seLigaCom  = [];
 
     let indexAux;
 
@@ -33,19 +13,13 @@ var factory = (elemento, linha) => {
 
     });
 
-    console.log(linha)
+    //console.log(linha)
 
     // colocando no array proximo todos os elementos que possui 1 na matriz
     linha.forEach(function(element, index){
 
         if (element == 1) {
-             // Se o indice dessa iteração é menor que o indiceAux, significa que o elemento é o anterior
-            if (index < indexAux) {
-                anterior.push(index);
-            } else {
-                 proximo.push(index);
-            }
-
+            seLigaCom.push(index);
         }
 
     });
@@ -54,14 +28,83 @@ var factory = (elemento, linha) => {
 
     return {
         elemento: elemento,
-        anterior: anterior,
-        proximo: proximo,
-        visiado: false
+        seLigaCom: seLigaCom,
+        visitado: false,
+        filhosVisitados: false
     }
 };
 
-console.log(linha2);
+
+var search = (elementoBusca, matriz) => {
+
+    let linhaInicial = matriz[0];
+
+    let aux = true;
+
+    while(aux) {
+
+        linhaInicial.visitado = true;
+
+        if (linhaInicial.elemento == elementoBusca) {
+            console.log('É O ELEMENTO');
+        } else {
+
+            let auxLoop = 0;
+
+            while(auxLoop < linhaInicial.seLigaCom.length) {
+
+                if (!matriz[auxLoop].visitado) {
+                    
+                    console.log(matriz[auxLoop]);
+
+                    let auxLoop2 = 0;
+
+                    while(auxLoop2 < matriz[auxLoop].seLigaCom.length) {
+
+                        if (!matriz[auxLoop].seLigaCom[auxLoop2].visitado) {
+                            console.log(matriz[auxLoop].seLigaCom[auxLoop2]);
+                        }
+                        
+
+                        auxLoop2++;
+                    }
+                    
+
+                }
+                
+
+                auxLoop++;
+            }
+
+        }
+
+
+        aux = false;
+    }
+
+};
+
+var elementos = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+           /* A  B  C  D  E  F  G */
+var linhaA = [0, 1, 1, 0, 1, 0, 0];
+var linhaB = [1, 0, 0, 1, 0, 1, 0];
+var linhaC = [1, 0, 0, 0, 0, 0, 1];
+var linhaD = [0, 1, 0, 0, 0, 0, 0];
+var linhaE = [1, 0, 0, 0, 0, 1, 0];
+var linhaF = [0, 1, 0, 0, 1, 0, 0];
+var linhaG = [0, 0, 1, 0, 0, 0, 0];
+
+var linha1 = factory(elementos[0], linhaA);
+var linha2 = factory(elementos[1], linhaB);
+var linha3 = factory(elementos[2], linhaC);
+var linha4 = factory(elementos[3], linhaD);
+var linha5 = factory(elementos[4], linhaE);
+var linha6 = factory(elementos[5], linhaF);
+var linha7 = factory(elementos[6], linhaG);
 
 var matriz = [];
 
 matriz.push(linha1, linha2, linha3, linha4, linha5, linha6, linha7);
+
+search('B', matriz);
